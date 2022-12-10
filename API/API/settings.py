ba25ba66ad5095d12facdadc2cef4ffa73ae82a6
Abrikoos <45069148+Triple1312@users.noreply.github.com>
2022-12-10 15:50:38 +0100

    frontend source code pushed

--- API/API/settings.py
+++ API/API/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,13 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'TrainingAPI',
-    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,14 +133,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-]
 
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
---
+++ src/client/src/Page.js
@@ -0,0 +1,65 @@
+import "./page.css";
+import axios from "axios";
+import upload from "./upload.svg";
+import {useState} from "react";
+
+export default function Page(props) {
+    const [state, setState] = useState("drop");
+    const [abstract, setAbstract] = useState("");
+
+    function postFile(event) {
+        event.preventDefault();
+        const file = event.target.files[0];
+        const formData = new FormData();
+        formData.append('file', file, {contentType: 'application/pdf'});
+        console.log("post", formData);
+        axios.post('http://localhost:8000/apiv1/document/', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then((response) => {
+            setAbstract(response.data);
+            setState("result");
+        }).catch((error) => {console.log(error); setState("drop"); alert("Error: " + error)});
+        setState("loading");
+    }
+
+    function drawState() {
+        if (state === "drop") {
+            return (
+                <div className="content" onDrop={postFile}>
+                    <img src={upload} style={{height: "15vh"}} alt="upload icon" id="upload-icon" />
+                    <h3>Drag and drop your pdf here</h3>
+                    <input type="file" id="file-input" onChange={postFile}/>
+                </div>
+            );
+        }
+        else if (state === "loading") {
+            return (
+                <div className="content">
+                    <h2>Processing...</h2>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div>
+                    <h3>Abstract</h3>
+                    <br/>
+                    <p>{abstract}</p>
+                </div>
+            )
+
+        }
+    }
+
+    return (
+        <div style={{backgroundColor:"#282c34"}}>
+            <div id="topbar" >
+                <h2>IR Front End</h2>
+                {(state === "result") ? <button onClick={() => setState("drop")}>Back</button> : null}
+            </div>
+            {drawState()}
+        </div>
+    )
+}
