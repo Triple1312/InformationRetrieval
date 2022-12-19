989d049e185282b94529cde661ecb08da7e9c671
Abrikoos <45069148+Triple1312@users.noreply.github.com>
2022-12-19 15:34:51 +0100

    frontend

--- src/client/src/Page.js
+++ src/client/src/Page.js
@@ -7,13 +7,13 @@ export default function Page(props) {
     const [state, setState] = useState("drop");
     const [abstract, setAbstract] = useState("");
 
-    function postFile(event) {
-        event.preventDefault();
-        const file = event.target.files[0];
+    function postFile() {
+        const file = document.getElementById("file-input").files[0]
+        const index = document.getElementById("summary-value").value
         const formData = new FormData();
         formData.append('file', file, {contentType: 'application/pdf'});
         console.log("post", formData);
-        axios.post('http://localhost:8000/apiv1/predict', formData, {
+        axios.post('http://localhost:8000/apiv1/predict/' + index, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
@@ -24,13 +24,39 @@ export default function Page(props) {
         setState("loading");
     }
 
+    function postJaccard() {
+        const file = document.getElementById("file-input").files[0]
+        const index = document.getElementById("summary-value").value
+        const formData = new FormData();
+        formData.append('file', file, {contentType: 'application/pdf'});
+        console.log("post", formData);
+        axios.post('http://localhost:8000/apiv1/train/' + index, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then((response) => {
+            setAbstract(response.data.Jaccard);
+            console.log(response.data);
+            setState("Jaccard");
+        }).catch((error) => {console.log(error); setState("drop"); alert("Error: " + error)});
+        setState("loading");
+    }
+
     function drawState() {
         if (state === "drop") {
             return (
                 <div className="content" onDrop={postFile}>
                     <img src={upload} style={{height: "15vh"}} alt="upload icon" id="upload-icon" />
                     <h3>Drag and drop your pdf here</h3>
-                    <input type="file" id="file-input" onChange={postFile}/>
+                    <input type="file" id="file-input" />
+                    <br/>
+                    <label htmlFor="summary-value">Summary amount:</label>
+                    <input type="text" name="summary-value" id="summary-value" defaultValue="0.2"/>
+                    <br/>
+                    <div style={{display: "flex"}}>
+                        <button style={{margin: "10px"}} onClick={() => postFile()}>Abstract</button>
+                         <button style={{margin: "10px"}} onClick={() => postJaccard()}>Jaccard</button>
+                    </div>
                 </div>
             );
         }
@@ -41,12 +67,21 @@ export default function Page(props) {
                 </div>
             );
         }
+        else if (state === "Jaccard") {
+            return (
+                <div className="content">
+                    <h2>Jaccard index</h2>
+                    <br/>
+                    <p style={{color: "ghostwhite", display: 'initial'}}>{abstract}</p>
+                </div>
+            )
+        }
         else {
             return (
-                <div >
-                    <h3>Abstract</h3>
+                <div style={{position: 'absolute', top: "10vh", left: "10vw", width: "80vw"}}>
+                    <h2 style={{color: "ghostwhite"}}>Abstract</h2>
                     <br/>
-                    <p style={{color: "ghostwhite"}}>{abstract}</p>
+                    <p style={{color: "ghostwhite", display: 'initial'}}>{abstract}</p>
                 </div>
             )
 
@@ -56,8 +91,8 @@ export default function Page(props) {
     return (
         <div style={{backgroundColor:"#282c34", minHeight: "100vh"}}>
             <div id="topbar" >
-                <h2>IR Front End</h2>
-                {(state === "result") ? <button onClick={() => setState("drop")}>Back</button> : null}
+                <h2>Information Retrieval: Paper summarizer</h2>
+                {(state === "result" || state === "Jaccard") ? <button onClick={() => setState("drop")}>Back</button> : null}
             </div>
             {drawState()}
         </div>
